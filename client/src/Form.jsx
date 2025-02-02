cf09b41669ee334ec62ccbaf4209a8ce96639f05
zakuma1 <47258376+zakuma1@users.noreply.github.com>
2025-02-02 06:38:01 +0200

    added responsiveness

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,107 +1,95 @@
-import './Form.css'
+import './Form.css';
 import { FaEye } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import CheckboxWithCheckmark from './Checkbox';
 
-
-
-
 export default function Form() {
-
     const [showPassword, setShowPassword] = useState(false);
+    const passwordInputRef = useRef(null);
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
 
-    function toggleEye() {
-        setShowPassword((prevState) => !prevState);
+    function handleEmailChanges(e) {
+        setEmail(e.target.value);
     }
 
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+    function toggleEye() {
+        setShowPassword(prevState => !prevState);
+
+        passwordInputRef.current.focus();
+    }
 
     useEffect(() => {
-        // Apply the gradient background to the body
         document.body.classList.add('bg-gradient-to-r', 'from-stone-800', 'via-zinc-500', 'to-slate-700', 'min-h-screen');
-
-        // Clean up the effect when the component unmounts
         return () => {
             document.body.classList.remove('bg-gradient-to-r', 'from-stone-800', 'via-zinc-500', 'to-slate-700', 'min-h-screen');
         };
-    }, []); // Empty dependency array to run only onc
-
-
-
-
-
+    }, []);
 
     return (
-        <div className="container rounded flex flex-col pb-2 pl-2 pr-2 " >
-            <div className='header pt-20 pb-5 pl-10 pr-10'>
-                <p className='mb-5 text-2xl font-bold '>Sign in to your account</p>
-                <div className='emailLabel '>
-                    <span className='emailSpan '><label className='text-base' htmlFor="email">Email</label></span>
-
-                    <input type="text" className='text-base w-full p-2 rounded border-1 border-stone-500 focus:border-blue-500 focus:ring-3 focus:ring-blue-300  transition-outline duration-300 ' />
+        <form className="container rounded flex flex-col pb-2 pl-1 pr-1">
+            <div className='header pt-20 pb-5 pl-5 pr-5 sm:pl-10 sm:pr-10'>
+                <p className='mb-5 text-md sm:text-2xl font-bold '>Sign in to your account</p>
+                <div className='emailLabel'>
+                    <span className='emailSpan'><label className='text-xs sm:text-base' htmlFor="email">Email</label></span>
+                    <input
+                        value={email}
+                        onChange={handleEmailChanges}
+                        type="text"
+                        className='text-xs sm:text-base w-full p-2 rounded border-1 border-stone-500 focus:border-blue-500 focus:ring-3 focus:ring-blue-300 transition-outline duration-300'
+                    />
                 </div>
 
                 <div className='mb-2'>
-
                     <div className='flex flex-row justify-between mt-2 mb-2'>
+                        <div><span className='text-xs sm:text-base'>Password</span></div>
                         <div>
-                            <span className='text-base'>Password</span>
-                        </div>
-                        <div>
-                            <span className='text-purple-700 text-base hover:cursor-pointer' >Forgot your password?</span>
+                            <span className='text-purple-700 text-xs sm:text-base hover:cursor-pointer'>Forgot your password?</span>
                         </div>
                     </div>
 
                     <div className="passwordContainer mb-3" style={{ position: 'relative' }}>
                         <input
+                            value={password}
+                            onChange={handlePasswordChange}
+                            ref={passwordInputRef}
                             type={showPassword ? 'text' : 'password'}
-                            className='text-base w-full p-2 rounded border-1 border-stone-500 focus:border-blue-500 focus:ring-3 focus:ring-blue-300   transition-outline duration-300'
-
+                            className='text-xs sm:text-base w-full p-2 rounded border-1 border-stone-500 focus:border-blue-500 focus:ring-3 focus:ring-blue-300 transition-outline duration-300'
                         />
                         {showPassword ? (
                             <FaRegEyeSlash
                                 onClick={toggleEye}
-                                style={{
-                                    position: 'absolute',
-                                    right: '0.6em',
-                                    bottom: '0.25rem',
-                                    fontSize: '1.7rem',
-                                    cursor: 'pointer',
-                                    userSelect: 'none'
-                                }}
+                                className="absolute right-2 bottom-1.5 sm:right-3 sm:bottom-2 text-sm sm:text-xl cursor-pointer"
                             />
                         ) : (
                             <FaEye
                                 onClick={toggleEye}
-                                style={{
-                                    position: 'absolute',
-                                    right: '0.6em',
-                                    bottom: '0.25rem',
-                                    fontSize: '1.7rem',
-                                    cursor: 'pointer',
-                                    userSelect: 'none'
-                                }}
+                                className="absolute right-2 bottom-1.5 sm:right-3 sm:bottom-2 text-sm sm:text-xl cursor-pointer"
                             />
                         )}
                     </div>
-
                 </div>
+
                 <div className='flex items-center mt-1 mb-3'>
                     <CheckboxWithCheckmark />
-                    <span className='ml-2 text-base'>Remember me on this device</span>
+                    <span className='ml-2 text-xs sm:text-base'>Remember me on this device</span>
                 </div>
 
-                <div className='text-center mt-2   '>
-                    <button className='w-full text-base flex items-center justify-center bg-blue-600 text-white rounded hover:border-transparent hover:bg-blue-700 w-110 text-1xl h-10 hover:cursor-pointer active:ring-3 active:ring-blue-300'> Sign in</button>
+                <div className='text-center mt-2'>
+                    <button className='w-full text-xs sm:text-base flex items-center justify-center bg-blue-600 text-white rounded hover:border-transparent hover:bg-blue-700 w-110 text-1xl h-10 hover:cursor-pointer active:ring-3 active:ring-blue-300'>
+                        Sign in
+                    </button>
                 </div>
-
-
-
-
             </div>
-            <div className='text-base text-center mt-5 w-full bg-neutral-200 h-14 flex items-center justify-center mt-7  rounded'>
+
+            <div className='text-xs sm:text-base text-center mt-5 w-full bg-neutral-200 h-14 flex items-center justify-center mt-7 rounded'>
                 <p>Click <span className='text-purple-700 underline hover:text-purple-600 hover:cursor-pointer'>here</span> to make an account</p>
             </div>
-        </div>
-    )
+        </form>
+    );
 }
