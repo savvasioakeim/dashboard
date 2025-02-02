8fa09bc5923c03508f6e2896364383b2fb362d3c
zakuma1 <47258376+zakuma1@users.noreply.github.com>
2025-02-02 04:31:37 +0200

    added complete form

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -18,38 +18,39 @@ export default function Form() {
 
 
     return (
-        <div className="container">
-            <div className='header'>
-                <p className='mb-2 text-3xl'>Sign in to your account</p>
-                <div className='emailLabel'>
-                    <span className='emailSpan'><label htmlFor="email">Email</label></span>
+        <div className="container rounded flex flex-col pb-2 pl-2 pr-2 " >
+            <div className='header pt-20 pb-5 pl-10 pr-10'>
+                <p className='mb-5 text-3xl font-bold '>Sign in to your account</p>
+                <div className='emailLabel '>
+                    <span className='emailSpan '><label className='text-m' htmlFor="email">Email</label></span>
 
-                    <input type="text" />
+                    <input type="text" className='w-full p-2 rounded border-1 border-stone-500 focus:border-blue-500 focus:ring-3 focus:ring-blue-300  transition-outline duration-300' />
                 </div>
 
                 <div className='mb-2'>
 
                     <div className='passwordLabel'>
                         <div>
-                            <span>Password</span>
+                            <span className='text-m'>Password</span>
                         </div>
                         <div>
-                            <span className='forgotPasswordSpan' >Forgot your password?</span>
+                            <span className='forgotPasswordSpan text-m' >Forgot your password?</span>
                         </div>
                     </div>
 
-                    <div className="passwordContainer" style={{ position: 'relative' }}>
+                    <div className="passwordContainer mb-3" style={{ position: 'relative' }}>
                         <input
                             type={showPassword ? 'text' : 'password'}
-                            placeholder="Enter your password"
+                            className='w-full p-2 rounded border-1 border-stone-500 focus:border-blue-500 focus:ring-3 focus:ring-blue-300   transition-outline duration-300'
+
                         />
                         {showPassword ? (
                             <FaRegEyeSlash
                                 onClick={toggleEye}
                                 style={{
                                     position: 'absolute',
-                                    right: '1em',
-                                    bottom: '0.6rem',
+                                    right: '0.8em',
+                                    bottom: '0.5rem',
                                     fontSize: '1.7rem',
                                     cursor: 'pointer',
                                     userSelect: 'none'
@@ -60,8 +61,8 @@ export default function Form() {
                                 onClick={toggleEye}
                                 style={{
                                     position: 'absolute',
-                                    right: '1em',
-                                    bottom: '0.6rem',
+                                    right: '0.8em',
+                                    bottom: '0.5rem',
                                     fontSize: '1.7rem',
                                     cursor: 'pointer',
                                     userSelect: 'none'
@@ -75,10 +76,16 @@ export default function Form() {
                     <CheckboxWithCheckmark />
                 </div>
 
+                <div className='text-center mt-2   '>
+                    <button className='flex items-center justify-center bg-blue-600 text-white rounded hover:border-transparent hover:bg-blue-700 w-110 text-1xl h-10 hover:cursor-pointer active:ring-3 active:ring-blue-300'> Sign in</button>
+                </div>
 
 
 
 
+            </div>
+            <div className='text-center mt-5 w-full bg-neutral-200 h-14 flex items-center justify-center mt-7  rounded'>
+                <p>Click <span className='text-purple-400 underline hover:text-purple-600 hover:cursor-pointer'>here</span> to make an account</p>
             </div>
         </div>
     )
